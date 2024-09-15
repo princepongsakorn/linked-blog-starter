072f990ad4d86b451c9cd4ea4842ba62491762e7
Pongsakorn P <pongsakorn@tokenx.finance>
2024-09-15 22:57:46 +0700

    Update header name and footer

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -1,31 +1,29 @@
-import PreviewLink from "./preview-link"
-
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-neutral-50">
       <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <PreviewLink href="/statically-generated">Statically Generated</PreviewLink> with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="#"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/matthewwong525/linked-blog-starter`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+        <div className="py-10 text-[#242424] flex flex-col items-center">
+          <div className="text-center flex flex-col justify-center items-center">
+            <p>Written by Pongsakorn Pongsutiyakorn</p>
+            <div className="flex flex-row">
+              <a
+                href={`https://github.com/matthewwong525/linked-blog-starter`}
+                className="mx-1 font-bold hover:underline"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://www.linkedin.com/in/pongsakorn-pongsutiyakorn-a55242198/"
+                className="mx-1 font-bold hover:underline"
+              >
+                Linkedin
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
