7dd0ae3700f596c140a51635eb477ff2a0393ba6
Pongsakorn P <pongsakorn@tokenx.finance>
2024-09-16 22:02:20 +0700

    Update color

--- components/blog/post-preview.tsx
+++ components/blog/post-preview.tsx
@@ -27,6 +27,8 @@ const PostPreview = ({
         </header>
         <div className="text-lg text-gray-600 mb-4 text-ellipsis">
           {excerpt.slice(0, 500)}
+        <div className="text-lg text-gray-600 mb-4 text-ellipsis italic">
+          {excerpt.slice(0, 250)}...
         </div>
         <footer className="text-sm">
           <PostMeta date={date} author={author} />
@@ -34,7 +36,7 @@ const PostPreview = ({
       </div>
       <Link as={`/${slug}`} href="/[...slug]" className="block shrink-0 ml-6">
         <span className="sr-only">Read more</span>
-        <svg className="w-4 h-4 fill-current text-blue-600" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-4 h-4 fill-current text-[#001165]" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
           <path d="M9.3 14.7l-1.4-1.4L12.2 9H0V7h12.2L7.9 2.7l1.4-1.4L16 8z" />
         </svg>
       </Link>
--- components/misc/search.tsx
+++ components/misc/search.tsx
@@ -72,7 +72,7 @@ function Search({ visible, setVisible }) {
         <div className="w-full">
           <label className="block text-sm sr-only" htmlFor="search">Search</label>
           <div className="relative flex items-center">
-            <input ref={inputRef} id="search" type="search" className="form-input w-full text-gray-800 px-3 py-2 pl-10" placeholder="Search my notes" onChange={handleChangeInput}/>
+            <input ref={inputRef} id="search" type="search" className="form-input w-full text-gray-800 px-3 py-2 pl-10 outline-[#03003c]" placeholder="Search my notes" onChange={handleChangeInput}/>
             <button type="submit" className="absolute inset-0 right-auto" aria-label="Search">
               <svg className="w-4 h-4 fill-current text-gray-400 mx-3 shrink-0" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                 <path d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5zM15.707 14.293L13.314 11.9a8.019 8.019 0 01-1.414 1.414l2.393 2.393a.997.997 0 001.414 0 .999.999 0 000-1.414z" />
